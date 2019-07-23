f50cc3f75802cdc8eb16f11a145f7522019ab5fc
Illeas <illeashossain34@gmail.com>
2019-07-24 01:07:33 +0600

    update

--- STL/Vector/vector/main.cpp
+++ STL/Vector/vector/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> v;
+
+    for(int i=1; i<=10;i++){
+        v.push_back(i);
+    }
+    for(int i=0;i!=v.size();i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
